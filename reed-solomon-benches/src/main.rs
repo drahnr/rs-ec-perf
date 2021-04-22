9d3cdfa392d69de6c237324cfc153300b45374e9
Bernhard Schuster <bernhard@ahoi.io>
2021-04-22 16:15:54 +0200

    fix/ci: fuzz and flamegraph jobs for PRs (#22)

--- reed-solomon-benches/src/main.rs
+++ reed-solomon-benches/src/main.rs
@@ -5,14 +5,14 @@ fn main() -> Result<()> {
 	color_eyre::install()?;
 
 	#[allow(unused)]
-	use reed_solomon_benches::{BYTES, N_SHARDS, TEST_DATA_CHUNK_SIZE};
+	use reed_solomon_benches::{BYTES, N_SHARDS};
 
 	{
 		use reed_solomon_benches::novelpoly;
 		reed_solomon_tester::roundtrip(
 			novelpoly::encode::<WrappedShard>,
 			novelpoly::reconstruct::<WrappedShard>,
-			&BYTES[..TEST_DATA_CHUNK_SIZE],
+			&BYTES[..],
 			N_SHARDS,
 		)?;
 	}
@@ -20,12 +20,7 @@ fn main() -> Result<()> {
 	#[cfg(feature = "novelpoly-with-alt-cxx-impl")]
 	{
 		use reed_solomon_benches::novelpoly;
-		reed_solomon_tester::roundtrip(
-			novelpoly::cxx::encode,
-			novelpoly::cxx::reconstruct,
-			&BYTES[..TEST_DATA_CHUNK_SIZE],
-			N_SHARDS,
-		)?;
+		reed_solomon_tester::roundtrip(novelpoly::cxx::encode, novelpoly::cxx::reconstruct, &BYTES[..], N_SHARDS)?;
 	}
 
 	#[cfg(feature = "naive")]
@@ -34,7 +29,7 @@ fn main() -> Result<()> {
 		reed_solomon_tester::roundtrip(
 			naive::encode::<WrappedShard>,
 			naive::reconstruct::<WrappedShard>,
-			&BYTES[..TEST_DATA_CHUNK_SIZE],
+			&BYTES[..],
 			N_SHARDS,
 		)?;
 	}
